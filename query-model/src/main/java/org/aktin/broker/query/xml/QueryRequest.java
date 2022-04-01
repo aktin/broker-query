0c12fe35bac2889fa092c880f7d3a0ed2d94af8f
Alexander Kombeiz <alexkombie@gmail.com>
2022-04-01 11:19:39 +0200

    added Constructors, formatting

--- query-model/src/main/java/org/aktin/broker/query/xml/QueryRequest.java
+++ query-model/src/main/java/org/aktin/broker/query/xml/QueryRequest.java
@@ -1,13 +1,8 @@
 package org.aktin.broker.query.xml;
 
+import javax.xml.bind.annotation.*;
 import java.time.Instant;
 import java.time.Period;
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  * Request execution of a query. The query is contained in this request type.
@@ -19,109 +14,126 @@ import javax.xml.bind.annotation.XmlTransient;
  * </p>
  *
  * @author R.W.Majeed
- *
  */
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class QueryRequest {
-	public static final String MEDIA_TYPE="application/vnd.aktin.query.request+xml";
-	/**
-	 * Unique id for the query request.
-	 * This id will always be unique and different
-	 * for multiple (e.g. recurring) requests for 
-	 * the same query.
-	 * The id is given by the broker.
-	 */
-	@XmlElement(required=true)
-	int id;
+    public static final String MEDIA_TYPE = "application/vnd.aktin.query.request+xml";
+    /**
+     * Unique id for the query request.
+     * This id will always be unique and different
+     * for multiple (e.g. recurring) requests for
+     * the same query.
+     * The id is given by the broker.
+     */
+    @XmlElement(required = true)
+    int id;
+    /**
+     * Date reference for queries. A recurring
+     * query may be requested once per month. The reference
+     * date will then be used to determine the time frame
+     * for the query. In other words, the reference date is used
+     * to fill a placeholder in the query syntax.
+     */
+    @XmlElement(required = true)
+    Instant reference;
+    /**
+     * Time stamp for the earliest execution / when the request is open.
+     * If unspecified, the query can be executed at any time (usually
+     * before the deadline)
+     */
+    @XmlElement(required = false)
+    Instant scheduled;
+    /**
+     * Due date until which the query results have to be submitted.
+     */
+    @XmlElement(required = true)
+    Instant deadline;
+    /**
+     * Query to execute
+     */
+    @XmlElement(required = true)
+    Query query;
+    @XmlElement(name = "signature", required = false)
+    Signature[] signatures;
 
-	public int getId(){
-		return id;
-	}
+    /**
+     * Default Constructor to unpack a received queries
+     */
+    public QueryRequest() {
+    }
 
-	@XmlTransient
-	public Integer getQueryId(){
-		if( getQuery().schedule instanceof RepeatedExecution ){
-			return ((RepeatedExecution)getQuery().schedule).id;
-		}else{
-			return null;
-		}
-	}
+    /**
+     * Argument Constructor to create queries
+     */
+    public QueryRequest(int id, Instant reference, Instant scheduled, Instant deadline, Query query) {
+        this.id = id;
+        this.reference = reference;
+        this.scheduled = scheduled;
+        this.deadline = deadline;
+        this.query = query;
+    }
 
-	@XmlTransient
-	public Period getQueryInterval() {
-		if (getQuery().schedule instanceof RepeatedExecution)
-			return ((RepeatedExecution) getQuery().schedule).interval;
-		else
-			return null;
+    public int getId() {
+        return id;
+    }
 
-	}
+    @XmlTransient
+    public Integer getQueryId() {
+        if (getQuery().schedule instanceof RepeatedExecution) {
+            return ((RepeatedExecution) getQuery().schedule).id;
+        } else {
+            return null;
+        }
+    }
 
-	@XmlTransient
-	public int getQueryIntervalHours() {
-		if (getQuery().schedule instanceof RepeatedExecution)
-			return ((RepeatedExecution) getQuery().schedule).intervalHours;
-		else
-			return 0;
+    @XmlTransient
+    public Period getQueryInterval() {
+        if (getQuery().schedule instanceof RepeatedExecution)
+            return ((RepeatedExecution) getQuery().schedule).interval;
+        else
+            return null;
 
-	}
+    }
 
+    @XmlTransient
+    public int getQueryIntervalHours() {
+        if (getQuery().schedule instanceof RepeatedExecution)
+            return ((RepeatedExecution) getQuery().schedule).intervalHours;
+        else
+            return 0;
 
-	/**
-	 * Timestamp for the earliest execution of the
-	 * query.
-	 * @return timestamp
-	 */
-	public Instant getScheduledTimestamp(){
-		return scheduled;
-	}
-	/**
-	 * Reference timestamp to use for data extraction.
-	 * This will be combined with duration contained in the execution schedule
-	 * to obtain the start and end timestamps of the queried data.
-	 * @return timestamp
-	 */
-	public Instant getReferenceTimestamp(){
-		return reference;
-	}
-	/**
-	 * Date reference for queries. A recurring
-	 * query may be requested once per month. The reference
-	 * date will then be used to determine the time frame
-	 * for the query. In other words, the reference date is used
-	 * to fill a placeholder in the query syntax.
-	 */
-	@XmlElement(required=true)
-	Instant reference;
-	
-	/**
-	 * Time stamp for the earliest execution / when the request is open.
-	 * If unspecified, the query can be executed at any time (usually
-	 * before the deadline)
-	 */
-	@XmlElement(required=false)
-	Instant scheduled;
-	/**
-	 * Due date until which the query results have to be submitted.
-	 */
-	@XmlElement(required=true)
-	Instant deadline;
+    }
+
 //	/**
 //	 * Date when the request was canceled. This indicates abnormal
 //	 * or unsuccessful termination of the request.
 //	 * A request can not be closed and canceled at the same time.
 //	 */
 //	Instant canceled;
-	
-	/**
-	 * Query to execute
-	 */
-	@XmlElement(required=true)
-	Query query;
 
-	public Query getQuery(){
-		return query;
-	}
-	@XmlElement(name="signature", required=false)
-	Signature[] signatures;
+    /**
+     * Timestamp for the earliest execution of the
+     * query.
+     *
+     * @return timestamp
+     */
+    public Instant getScheduledTimestamp() {
+        return scheduled;
+    }
+
+    /**
+     * Reference timestamp to use for data extraction.
+     * This will be combined with duration contained in the execution schedule
+     * to obtain the start and end timestamps of the queried data.
+     *
+     * @return timestamp
+     */
+    public Instant getReferenceTimestamp() {
+        return reference;
+    }
+
+    public Query getQuery() {
+        return query;
+    }
 }
